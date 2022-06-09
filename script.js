62a29d3cfb08cd27e61344774bd53671f58bf5ca
Franciane Lark <franciane.lark019@gmail.com>
2022-06-09 15:19:24 -0400

    update script

---
+++ script.js
@@ -0,0 +1,14 @@
+let nome = "Franciane";
+let numero = 28;
+let anoAtual = 2022 - numero;
+let soma = anoAtual + numero;
+
+console.log(soma);
+
+console.log("Estou mexendo no console.log");
+
+let quartaFeira = true;
+
+console.log(quartaFeira);
+
+console.log(typeof quartaFeira);
